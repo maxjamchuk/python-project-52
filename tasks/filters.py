749d759330ac696a13d1d0a199cfaa426969c143
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-12-02 02:32:17 +0500

    stage: 7

---
+++ tasks/filters.py
@@ -0,0 +1,43 @@
+from django import forms
+import django_filters
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth import get_user_model
+from statuses.models import Status
+from labels.models import Label
+from .models import Task
+
+User = get_user_model()
+
+
+class TaskFilter(django_filters.FilterSet):
+    status = django_filters.ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        label=_("Status"),
+    )
+
+    executor = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        label=_("Executor"),
+    )
+
+    label = django_filters.ModelChoiceFilter(
+        field_name="labels",
+        queryset=Label.objects.all(),
+        label=_("Label"),
+    )
+
+    self_tasks = django_filters.BooleanFilter(
+        method="filter_self_tasks",
+        widget=forms.CheckboxInput,
+        label=_("Only my tasks"),
+    )
+
+    class Meta:
+        model = Task
+        fields = ["status", "executor", "label"]
+
+    def filter_self_tasks(self, queryset, name, value):
+        if value and self.request and self.request.user.is_authenticated:
+            return queryset.filter(author=self.request.user)
+        return queryset
